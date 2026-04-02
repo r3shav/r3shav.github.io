aff899a08874c53d74e37a7c13c22e4701516983
r3shav <loader10@proton.me>
2026-04-02 13:28:08 +0530

    clean build

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1775115540',resources: [
+    cacheName: 'chirpy-1775116615',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
